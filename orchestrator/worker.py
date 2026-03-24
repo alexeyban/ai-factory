b73abf2c7e1b42564d10b3438dec4725c6773315
alexeyban <alexeyban@gmail.com>
2026-03-24 18:21:10 +0100

    auto: update worker.py"

--- orchestrator/worker.py
+++ orchestrator/worker.py
@@ -4,7 +4,7 @@ import logging
 from temporalio.client import Client
 from temporalio.worker import Worker
 
-from orchestrator.workflows import OrchestratorWorkflow, ProjectWorkflow
+from orchestrator.workflows import OrchestratorWorkflow, ProjectWorkflow, LearningWorkflow
 from orchestrator.activities import (
     pm_activity,
     pm_recovery_activity,
@@ -14,6 +14,8 @@ from orchestrator.activities import (
     qa_activity,
     analyst_activity,
     cleanup_stale_branches_activity,
+    extract_skill_activity,
+    policy_update_activity,
     process_single_task,
     process_all_tasks,
     dev_task,
